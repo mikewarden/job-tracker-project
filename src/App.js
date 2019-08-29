b063b818b0272a5e5c2378c2fb44be6a31e3bf19
Ange Miklos <angejuba@gmail.com>
2019-08-29 15:49:47 -0700

    added details fields

--- src/App.js
+++ src/App.js
@@ -3,7 +3,94 @@ import Form from './Form.js';
 import './App.css';
 
 class Post extends React.Component {
-	
+    
+    streetAddress = (addr, pStyle) => {
+      if (addr) {
+         return <p style={pStyle}>Address: {addr}</p>
+      } else {
+        return <p style={{display : "none"}}></p>
+      }
+    }
+
+    cityState = (city, pStyle) => {
+      if (city) {
+         return <p style={pStyle}>City, State: {city}</p>
+      } else {
+        return <p style={{display : "none"}}></p>
+      }
+    }
+
+    zipcode = (zip, pStyle) => {
+      if (zip) {
+         return <p style={pStyle}>Zip: {zip}</p>
+      } else {
+        return <p style={{display : "none"}}></p>
+      }
+    }
+
+    contactName = (name, pStyle) => {
+      if (name) {
+         return <p style={pStyle}>Contact: {name}</p>
+      } else {
+        return <p style={{display : "none"}}></p>
+      }
+    }
+
+    contactNumber = (num, pStyle) => {
+      if (num) {
+         return <p style={pStyle}>Cell: {num}</p>
+      } else {
+        return <p style={{display : "none"}}></p>
+      }
+    }
+
+    interviewDate = (interview, pStyle) => {
+      if (interview) {
+         return <p style={pStyle}>Interview Date: {interview}</p>
+      } else {
+        return <p style={{display : "none"}}></p>
+      }
+    }
+
+    phoneCallDate = (callD, pStyle) => {
+      if (callD) {
+         return <p style={pStyle}>Contacted: {callD}</p>
+      } else {
+        return <p style={{display : "none"}}></p>
+      }
+    }
+
+    positionId = (id, pStyle) => {
+      if (id) {
+         return <p style={pStyle}>Job Id: {id}</p>
+      } else {
+        return <p style={{display : "none"}}></p>
+      }
+    }
+
+    positionUrl = (url, pStyle) => {
+      if (url) {
+         return <p style={pStyle}>url: {url}</p>
+      } else {
+        return <p style={{display : "none"}}></p>
+      }
+    }
+
+    positionSalary = (salary, pStyle) => {
+      if (salary) {
+         return <p style={pStyle}>Salary: {salary}</p>
+      } else {
+        return <p style={{display : "none"}}></p>
+      }
+    }
+
+    positionDeadline = (deadline, pStyle) => {
+      if (deadline) {
+         return <p style={pStyle}>Deadline: {deadline}</p>
+      } else {
+        return <p style={{display : "none"}}></p>
+      }
+    }
 
    // dynamic style!
    listStyle = () => {
@@ -18,7 +105,12 @@ class Post extends React.Component {
    };
   
 	render() { 
-      const {id, filter, postType, compName, posTitle, date} = this.props.p;
+
+      const {id, filter, postType, compName, compSA, compCS, compZip, 
+             cNumber, cName, invwDate, pcDate, posTitle, posId, posUrl, 
+             salary, posDead, date} = this.props.p;
+
+      const pStyle = { textAlign : "center"};
 
       const btn1Style = {
             background : "black",
@@ -38,6 +130,7 @@ class Post extends React.Component {
                 float  : "left"
       };
 
+
 	    return (
         <div>
 	      <li className={ filter ? "nopost" : "post"} style={this.listStyle()}>
@@ -45,11 +138,20 @@ class Post extends React.Component {
           <button onClick={this.props.filterPost.bind(this, postType)} style={btn2Style}>F</button>
 		      <details>
 			      	<summary>{compName}</summary>
-			      	<p>address, contact, etc.</p>
+              {this.streetAddress(compSA, pStyle)}
+              {this.cityState(compCS, pStyle)}
+              {this.zipcode(compZip, pStyle)}
+              {this.contactName(cNumber, pStyle)}
+              {this.contactNumber(cName, pStyle)}
+              {this.interviewDate(invwDate, pStyle)}
+              {this.phoneCallDate(pcDate, pStyle)}
 		      </details>
 		      <details>
 			      	<summary>{posTitle}</summary>
-			      	<p>I have no idea</p>
+              {this.positionId(posId, pStyle)}
+              {this.positionUrl(posUrl, pStyle)}
+              {this.positionSalary(salary, pStyle)}
+              {this.positionDeadline(posDead, pStyle)}
 		      </details>
           <p>{date}</p>
 	      </li>
@@ -89,10 +191,10 @@ class JobTracker extends React.Component {
             compSA   : "123 Main St.",
             compCS   : "Dallas, Texas",
             compZip  : 75229,
-            CNumber  : "480-294-0824",
-            CName    : "Melissa Stines",
-            InvwDate : "12/12/2019",
-            PCDate   : "01/13/2020",
+            cNumber  : "480-294-0824",
+            cName    : "Melissa Stines",
+            invwDate : "12/12/2019",
+            pcDate   : "01/13/2020",
             posTitle : "Position Title Here",
             posId    : "#2346A876",
             posUrl   : "google.com",
@@ -104,14 +206,36 @@ class JobTracker extends React.Component {
             filter   : false,
             postType : "applied",
             compName : "Company Name Here",
+            compSA   : "123 Main St.",
+            compCS   : "Dallas, Texas",
+            compZip  : 75229,
+            cNumber  : "480-294-0824",
+            cName    : "Melissa Stines",
+            invwDate : "12/12/2019",
+            pcDate   : "01/13/2020",
             posTitle : "Position Title Here",
+            posId    : "#2346A876",
+            posUrl   : "google.com",
+            salary   : "$100k",
+            posDead  : Date("12/14/2015"),
             date     : Date()
         },{
             id       : 2,
             filter   : false,
             postType : "contacted",
             compName : "Company Name Here",
+            compSA   : "123 Main St.",
+            compCS   : "Dallas, Texas",
+            compZip  : 75229,
+            cNumber  : "480-294-0824",
+            cName    : "Melissa Stines",
+            invwDate : "12/12/2019",
+            pcDate   : "01/13/2020",
             posTitle : "Position Title Here",
+            posId    : "#2346A876",
+            posUrl   : "google.com",
+            salary   : "$100k",
+            posDead  : Date("12/14/2015"),
             date     : Date()
         }
         ]
