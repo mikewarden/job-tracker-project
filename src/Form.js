5139a2cdcb3b1cfe777e87f02e8cfb9dec6eeee6
Mike <mycelius23@gmail.com>
2019-08-29 14:33:27 -0700

    added form validation and new input fields.

--- src/Form.js
+++ src/Form.js
@@ -7,43 +7,75 @@ class Form extends React.Component {
 		super(props);
 		this.state = {
 			company: "",
-			position: ""
+			street: "",
+			cityState: "",
+			zipCode: "",
+			phone: "",
+			contactName: "",
+			interviewDate: "",
+			phoneCallDate: "",
+			position: "",
+			website: "",
+			salary: "",
+			deadline: ""
 		}
 	}
-
 
 	onSubmit = (event) => {
 		event.preventDefault();
 		this.props.addPost(this.state);
-	    
-		this.setState({
+	    this.setState({
 			company: "",
-			position: ""
+			street: "",
+			cityState: "",
+			zipCode: "",
+			phone: "",
+			contactName: "",
+			interviewDate: "",
+			phoneCallDate: "",
+			position: "",
+			website: "",
+			salary: "",
+			deadline: ""
 		})
 	}
-
 
 	onChange = (event) => {
 		this.setState({
 			[event.target.name]: event.target.value
-
 		});
-		console.log(event.target.name);
 	}
 
-  render() {
+  	render() {
     return (
       <div className="Form">
-        
         <form onSubmit={this.onSubmit}>
-        	<input type="text" name="company" placeholder="Company Name" value={this.state.company} onChange={this.onChange}/>
+        	<input type="text" name="company" placeholder="Company Name" value={this.state.company} onChange={this.onChange} required/>
+        	<br/>
+        	<input type="text" name="street" placeholder="Street Address" value={this.state.street} onChange={this.onChange} />
+        	<br/>
+        	<input type="text" name="cityState" placeholder="City,State" value={this.state.cityState} onChange={this.onChange} />
+        	<br/>
+        	<input type="text" name="zipCode" placeholder="Zip Code" value={this.state.zipCode} onChange={this.onChange} />
+        	<br/>
+        	<input type="text" name="phone" placeholder="Phone Number" value={this.state.phone} onChange={this.onChange} />
+        	<br/>
+        	<input type="text" name="contactName" placeholder="Contact Name" value={this.state.contactName} onChange={this.onChange} />
+        	<br/>
+        	<input type="text" name="interviewDate" placeholder="Interview Date" value={this.state.interviewDate} onChange={this.onChange} />
+        	<br/>
+        	<input type="text" name="phoneCallDate" placeholder="Phone Call Date" value={this.state.phoneCallDate} onChange={this.onChange} />
         	<br/>
         	<input type="text" name="position" placeholder="Desired Position" value={this.state.position} onChange={this.onChange} />
         	<br/>
+        	<input type="text" name="website" placeholder="Website URL" value={this.state.website} onChange={this.onChange} />
+        	<br/>
+        	<input type="text" name="salary" placeholder="Salary" value={this.state.salary} onChange={this.onChange} />
+        	<br/>
+        	<input type="text" name="deadline" placeholder="Deadline" value={this.state.deadline} onChange={this.onChange} />
+        	<br/>
         	<input type="submit" className="submit-btn" value="Submit"/>
-
-        	
-        </form>
+		</form>
       </div>
     );
   }
